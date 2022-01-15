1aa382c8a05a330e9d0f89e3be6ebda1c1117699
ramen6315 <ramen6315@gmail.com>
2022-01-16 01:11:04 +0900

    feat : 템플릿 / 콜백 패턴 예제 연습

---
+++ src/main/java/com/example/tbspring/learning/template/CalculatorInterface.java
@@ -0,0 +1,8 @@
+package com.example.tbspring.learning.template;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public interface CalculatorInterface {
+    Integer calValue(BufferedReader bufferedReader) throws IOException;
+}
